6ba19ed1bb2a3cd4fb705a6cbee7f5ddc35384b1
irispsyence <irispsyence@gmail.com>
2026-03-05 14:34:29 -0500

    P: revert K scan.sh script
    This reverts commit 55ba9d81a0e786d84e144a8f3cfa9dd813f2e80d.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,15 +1,1 @@
-#Credit Cards
-printf "\n====== SCANNING FOR CREDIT CARD NUMBERS ======\n"
-grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "========= CREDIT CARD SCAN COMPLETE =========="
-
-#Social Security Numbers
-printf "\n==== SCANNING FOR SOCIAL SECURITY NUMBERS ====\n"
-grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "======= SOCIAL SECURITY SCAN COMPLETE ========"
-
-#Phone Numbers
-printf "\n========= SCANNING FOR PHONE NUMBERS =========\n"
-grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
-echo "========= PHONE NUMBER SCAN COMPLETE ========="
+# TODO: write the script
